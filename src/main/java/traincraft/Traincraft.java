352ca5f107dbd73511cba7ff8cc3367dd9dbff9a
euphonium.ashe.fin <euphonium.ashe.f@gmail.com>
2026-04-05 01:41:15 +0100

    Port to NeoForge 2.0 and register mod objects
    Update build scripts to net.neoforged.moddev 2.0.141, add java-library
    and maven-publish, and set parchment mappings to 2024.06.23 for
    Minecraft 1.21. Refactor Traincraft to accept an IEventBus, register
    blocks/items/armor/fluids/tiles/entities/menus/recipes, and add
    lifecycle
    listeners for common setup, creative tab contents, screen registration,
    and entity renderers.

--- src/main/java/traincraft/Traincraft.java
+++ src/main/java/traincraft/Traincraft.java
@@ -6,8 +6,28 @@
 package traincraft;
 
 import net.neoforged.fml.common.Mod;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import traincraft.blocks.TCBlocks;
+import traincraft.blocks.assemblytable.ScreenAssemblyTable;
+import traincraft.blocks.distillery.ScreenDistillery;
+import traincraft.blocks.openhearthfurnace.ScreenOpenHearthFurnace;
+import traincraft.blocks.trainworkbench.ScreenTrainWorkbench;
+import traincraft.entity.TCEntities;
+import traincraft.items.TCItems;
+import traincraft.items.armor.TCArmor;
+import traincraft.liquids.TCLiquids;
+import traincraft.network.TCMenus;
+import traincraft.recipe.DistilleryRecipe;
+import traincraft.recipe.TCRecipes;
+import traincraft.renderer.LocomotiveSteamSmallRenderer;
+import traincraft.tile.TCTiles;
 
 @Mod(Traincraft.MOD_ID)
 public class Traincraft {
@@ -17,9 +37,45 @@ public class Traincraft {
     public static final String MOD_VERSION = "5.0.0-alpha1";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
-    public Traincraft() {
-        LOGGER.info("Traincraft " + MOD_VERSION + " loaded!");
-        LOGGER.info("Rewrite in progress for NeoForge 1.21+");
-        LOGGER.info("Testing with runClient - registration works at runtime");
+    public Traincraft(IEventBus modEventBus) {
+        TCBlocks.BLOCKS.register(modEventBus);
+        TCItems.ITEMS.register(modEventBus);
+        TCArmor.ARMOR_ITEMS.register(modEventBus);
+        TCLiquids.FLUID_TYPES.register(modEventBus);
+        TCLiquids.FLUIDS.register(modEventBus);
+        TCTiles.TILES.register(modEventBus);
+        TCEntities.ENTITIES.register(modEventBus);
+        TCMenus.MENUS.register(modEventBus);
+        TCRecipes.RECIPE_SERIALIZERS.register(modEventBus);
+        TCRecipes.RECIPE_TYPES.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreativeTabContents);
+        modEventBus.addListener(this::registerScreens);
+        modEventBus.addListener(this::registerEntityRenderers);
+
+        LOGGER.info("Traincraft {} loaded!", MOD_VERSION);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Common setup complete");
+    }
+
+    private void addCreativeTabContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            TCItems.ITEMS.getEntries().forEach(holder -> event.accept(holder.get()));
+            TCArmor.ARMOR_ITEMS.getEntries().forEach(holder -> event.accept(holder.get()));
+        }
+    }
+
+    private void registerScreens(RegisterMenuScreensEvent event) {
+        event.register(TCMenus.DISTILLERY.get(), ScreenDistillery::new);
+        event.register(TCMenus.ASSEMBLY_TABLE.get(), ScreenAssemblyTable::new);
+        event.register(TCMenus.TRAIN_WORKBENCH.get(), ScreenTrainWorkbench::new);
+        event.register(TCMenus.OPEN_HEARTH_FURNACE.get(), ScreenOpenHearthFurnace::new);
+    }
+
+    private void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(TCEntities.LOCOMOTIVE_STEAM_SMALL.get(), LocomotiveSteamSmallRenderer::new);
     }
 }
